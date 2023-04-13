a9481913204ba25b0688c95a2403df56f98c6ef9
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-13 22:24:27 +0200

    arrays practice

--- array.js
+++ array.js
@@ -1,3 +1,13 @@
 var cars = ['Porsche', 'Audi', 'Mercedes', 'BMW', 'Wolkswagen'];
 console.log(cars);
 console.log(cars[3]);
+
+
+
+
+// var numbers = [20, 30, 43, 89, 90, 38];
+// var length = numbers.length;
+// console.log(length);
+
+
+
